e77c3da4a2160bdad0d26a05aebd0a012579986b
Nabin-sth <nabinshrestha120@gmail.com>
2025-01-01 09:03:46 +0545

    add getVideoComments endpoint

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -9,6 +9,16 @@ const getVideoComments = asyncHandler(async (req, res) => {
   //TODO: get all comments for a video
   const { videoId } = req.params;
   const { page = 1, limit = 10 } = req.query;
+  if (!videoId) {
+    throw new ApiError(400, "video id not found");
+  }
+  const skip = (page - 1) * limit;
+  const comment = await Comment.find({ video: videoId }).skip(skip);
+  // ById({ video: videoId });
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, comment, "All comments success."));
 });
 
 const addComment = asyncHandler(async (req, res) => {
@@ -44,33 +54,41 @@ const updateComment = asyncHandler(async (req, res) => {
   const { commentId } = req.params;
   const { newComment } = req.body;
 
-  if (!commentId || !newComment) {
-    throw new ApiError(400, "no comment found");
-  }
-  const comment = await Comment.findByIdAndUpdate(
-    commentId,
-    {
-      $set: { content: newComment },
-    },
-    {
-      new: true,
+  try {
+    if (!commentId || !newComment) {
+      throw new ApiError(400, "no comment found");
     }
-  );
-  return res
-    .status(200)
-    .json(new ApiResponse(200, comment, "Comment updated Successfully"));
+    const comment = await Comment.findByIdAndUpdate(
+      commentId,
+      {
+        $set: { content: newComment },
+      },
+      {
+        new: true,
+      }
+    );
+    return res
+      .status(200)
+      .json(new ApiResponse(200, comment, "Comment updated Successfully"));
+  } catch (error) {
+    throw new ApiError(400, "error while updating comment", error.message);
+  }
 });
 
 const deleteComment = asyncHandler(async (req, res) => {
   // TODO: delete a comment
   const { commentId } = req.params;
-  if (!commentId) {
-    throw new ApiError(400, "no comment found");
+  try {
+    if (!commentId) {
+      throw new ApiError(400, "no comment found");
+    }
+    const comment = await Comment.findByIdAndDelete(commentId);
+    return res
+      .status(200)
+      .json(new ApiResponse(200, comment, "comment deleted successfully"));
+  } catch (error) {
+    throw new ApiError(400, "error while deleting comment", error.message);
   }
-  const comment = await Comment.findByIdAndDelete(commentId);
-  return res
-    .status(200)
-    .json(new ApiResponse(200, comment, "comment deleted successfully"));
 });
 
 export { getVideoComments, addComment, updateComment, deleteComment };
